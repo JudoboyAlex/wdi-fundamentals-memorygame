bcac049b7aca286be690e287c118fe676f6e0cd4
Alexander G. Yang <judoboy@Alexanders-MacBook-Pro.local>
2019-04-03 14:11:09 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,17 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-
-var cards =['queen', 'queen', 'king', 'king'];
-var cardsInPlay=[];
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
-} else  { 
-    alert("Sorry, try again.")
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
 }
 
-
-
+function flipCard(cardId) {
+  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId]);
+  checkForMatch();
+}
+flipCard(1);
